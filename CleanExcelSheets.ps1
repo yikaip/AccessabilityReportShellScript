a028f7d3f89dbe29733c2a37e12d8bcb14d7896d
yikaip <ypeng@uwyo.edu>
2020-04-03 18:06:53 -0600

    we can now type in the name of the path to access it

--- CleanExcelSheets.ps1
+++ CleanExcelSheets.ps1
@@ -7,7 +7,11 @@
  #>
 
 
-
+ Write-Host "---------------------------------------------------------------------------------------------------------"
+ $originName = Read-Host "Please type in th file name, ex.288831"
+ $saveAsName = Read-Host "We need to save this file as .csv just for now. Please type in your temporary for this csv file"
+ $newName = Read-Host "Okay, One more step. Please type in the actual name of this file, ex.2020Q1-ANISCI."
+ Write-Host "---------------------------------------------------------------------------------------------------------"
 function ConvertExcelToCSV {
     #Replace this path with actual file location: ex. warehouse/2020Q1..., will improve it later
     #First thing is to open the excel sheet
@@ -34,16 +38,26 @@ function ConvertExcelToCSV {
 
 
 function Delete () {
-    $P = Import-Csv -Path "C:\Downloads\new.csv" -Delimiter "," | Where-Object  {
-        $_.Note -CNotMatch "This HEAD does not contain a title element" -and $_.Note -CNotMatch "This LINK has an id attribute of 'font-awesome-5-kit-css', which is not unique" -and $_.Note -CNotMatch "This INPUT has an id attribute of" -and $_.Note -CNotMatch "carouselSS"} | Export-Csv -Path "C:\Downloads\New1.csv" 
+    $P = Import-Csv -Path ("C:\Downloads\" + $saveAsName + ".csv")-Delimiter "," | Where-Object  {
+        $_.Note -CNotMatch "This HEAD does not contain a title element" -and $_.Note -CNotMatch "This LINK has an id attribute of 'font-awesome-5-kit-css', which is not unique" -and $_.Note -CNotMatch "This INPUT has an id attribute of" -and $_.Note -CNotMatch "carouselSS"} | Export-Csv -Path ("C:\Downloads\" + $newName +".csv")
                            #replace this path with actual file location  #delect all items that has "" 
 }
 
 function DoubleCheckViolations {
-    $P = Import-Csv -Path "C:\Downloads\new.csv" -Delimiter ","| Where-Object  {
-        $_.Note -Match "This P contains" -or $_.Note -Match "This H2"} | Export-Csv -Path "C:\Users\yikai\Downloads\New1.csv"
+    $P = Import-Csv -Path ("C:\Downloads\" + $saveAsName + ".csv") -Delimiter ","| Where-Object  {
+        $_.Note -Match "This P contains" -or $_.Note -Match "This H2"} | Export-Csv -Path ("C:\Downloads\" + $newName +".csv")
 }
 
+function ConvertCSVToExcel {
+    $csvFile = "C:\Downloads\" + $newName + ".csv"
+    $Excel = New-Object -ComObject Excel.Application
+    $wb = $Excel.WorkBooks.Open($csvFile)
+    $worksheet = $wb.Sheets.Item(1)
+    $worksheet.Cells.Item(1,1).EntireRow.Delete()
+    $wb.SaveAs("C:\Downloads\" + $newName + ".xlsx", 61)
+    $wb.close()
+    $Excel.Quit()
+}
 
 #--------------MAIN-------------#
 ConvertExcelToCSV
@@ -53,5 +67,5 @@ if ($Ans = "Violation"){
 } elseif ($Ans = "Needs Review") {
     DoubleCheckViolations
 }
-
-
+ConvertCSVToExcel
+Write-Host "---------------------------------------------------------------------------------------------------------"
